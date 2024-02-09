928a3f080e6dd9f0ad465d90bf7b4fe7c795f7fc
Michael Shipman <michael@shipman>
2024-02-09 14:04:11 -0500

    documented servo drive code

--- 2024-Firmware/2024.0/vent-servo.c
+++ 2024-Firmware/2024.0/vent-servo.c
@@ -5,34 +5,26 @@
 #include "hardware/pwm.h"
 #include "pico/stdlib.h"
 
-uint max_level = 1800 * 3;
+uint max_level = 1300 * 3;
 uint min_level = 600 * 3;
 uint slice_num = 0;
 uint pwm_pin = 0;
 
 void setup_vent_servo(uint pin) {
     pwm_pin = pin;
-    gpio_set_function(pwm_pin, GPIO_FUNC_PWM);
-
     slice_num = pwm_gpio_to_slice_num(pwm_pin);
 
+    gpio_set_function(pwm_pin, GPIO_FUNC_PWM);
     pwm_set_phase_correct(slice_num, true);
-
     pwm_set_clkdiv(slice_num, PWM_DIV);
-
     pwm_set_wrap(slice_num, WRAP_VALUE);
-
-    pwm_set_chan_level(slice_num, PWM_CHAN_B, min_level);
-
+    pwm_set_gpio_level(pwm_pin, min_level);
     pwm_set_enabled(slice_num, true);
 }
 
 void vent_servo_demo() {
-    uint level = 2100;  // def don't go past 2000, 600-1300 for normal
-                        // range of motion on UMD Vent
-
+    uint level = 2100;
     bool forward = false;
-
     float num_usec = 700;
 
     while (true) {
@@ -43,13 +35,11 @@ void vent_servo_demo() {
 
         pwm_set_gpio_level(pwm_pin, level);
 
-        // pwm_set_chan_level(slice_num, PWM_CHAN_B, num_usec);
-
         printf("microseconds: %f\n", num_usec);
 
-        if (level >= (1300 * 3)) forward = false;
+        if (level >= (max_level)) forward = false;
 
-        if (level <= (600 * 3)) forward = true;
+        if (level <= (min_level)) forward = true;
 
         if (forward) {
             num_usec = num_usec + 0.33;
@@ -63,7 +53,6 @@ void vent_servo_demo() {
     }
 }
 
-// level is actually in 1/3 microseconds
 short vent_servo_set_pulse_width(uint level) {
     if (level > max_level || level < min_level) {
         printf("PWM duty cycle out of bounds\n");
@@ -73,3 +62,6 @@ short vent_servo_set_pulse_width(uint level) {
     pwm_set_gpio_level(pwm_pin, level);
     return 0;
 }
+
+void vent_servo_open() { vent_servo_set_pulse_width(max_level); }
+void vent_servo_close() { vent_servo_set_pulse_width(min_level); }
--- 2024-Firmware/2024.0/vent-servo.h
+++ 2024-Firmware/2024.0/vent-servo.h
@@ -3,13 +3,43 @@
 
 #include "pico/stdlib.h"
 
-#define WRAP_VALUE (uint16_t)59999
-#define PWM_DIV 8
+// Below values assume a 48 MHz system clock, otherwise PWM frequency and pulse
+// length will be incorrect
+#define WRAP_VALUE (uint16_t)59999  // wrap value of PWM counter
+#define PWM_DIV 8                   // PWM clock divider, divides system clock
 
+/**
+ * @brief Configures PWM pin and peripheral, system clock must be set to 48 MHz
+ * before this function is called.
+ *
+ * @param pwm_pin
+ */
 void setup_vent_servo(uint pwm_pin);
+
+/**
+ * @brief Repeatedly opens and closes vent servo from the min angle to the max
+ * angle.
+ */
 void vent_servo_demo();
+
+/**
+ * Convenience function to set servo to maximum angle (vent open).
+ */
 void vent_servo_open();
+
+/**
+ * Convenience function to set servo to minimum angle (vent closed).
+ */
 void vent_servo_close();
-short vent_servo_set_pulse_width(uint num_usec);
+
+/**
+ * Convenience function to set servo level. Checks level against maximum and
+ * minimum allowable values to prevent collision.
+ *
+ * @param level length of servo PWM pulse length in 1/3 microseconds.
+ *
+ * @returns -1 if level is out of bounds, 0 on success
+ */
+short vent_servo_set_pulse_width(uint level);
 
 #endif
